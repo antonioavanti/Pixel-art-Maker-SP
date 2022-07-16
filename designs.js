03ee3fdd1015da474150154bc7da249927b3c8a0
ANTHONY GATHOGO <106891513+antonioavanti@users.noreply.github.com>
2022-07-15 20:02:52 -0500

    Setup

--- designs.js
+++ designs.js
@@ -1,5 +1,9 @@
 // Select color input
+let colorInput = document.getElementById('colorpicker');
+
 // Select size input
+let contain = document.querySelector("table");
+let submitButton = document.getElementById("sizepicker");
 
 // When size is submitted by the user, call makeGrid()
 
