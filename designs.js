46dd7a47285b3b526bc4dae9f275ce03fcdb4188
ANTHONY GATHOGO <106891513+antonioavanti@users.noreply.github.com>
2022-07-16 19:42:41 -0500

    working the DOM
    The pixel art project is still unfinished but once I get done with lesson 7, I should be in better shape.

--- designs.js
+++ designs.js
@@ -1,14 +1,37 @@
 // Select color input
-let colorInput = document.getElementById('colorpicker');
+let selector = document.getElementById("colorpicker");
+let submit = document.getElementById("sizePicker");
 
 // Select size input
-let contain = document.querySelector("table");
-let submitButton = document.getElementById("sizepicker");
 
-// When size is submitted by the user, call makeGrid()
+//When size is submitted by the user, call makeGrid()
+submit.addEventListener("click", makeGrid());
+
 
 function makeGrid() {
 
-// Your code goes here!
+let row = document.getElementById("inputWidth").value;
+let col = document.getElementById("inputHeight").value;
+let grid = document.getElementById("pixelCanvas");
 
+for (var c = 0; c < row; c++) {
+	let row = document.createElement("tr");
+	row.id = "row" + c;
+
+	grid.appendChild(row);
+	let lat = document.getElementById("row" + c);
+
+	for (var d = 0; d < col; d++) {
+		let col = document.createElement("td");
+		lat.appendChild(col);
+	}
+  }
+  event.preventDefault();
+};
+
+// painting
+function setColor(color) {
+	color = selector.val();
+	return color;
 }
+
